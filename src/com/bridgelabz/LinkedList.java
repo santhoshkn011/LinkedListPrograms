61db2b7ade66854607c4c248ab344f84fed46507
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-30 18:15:24 +0530

    Added: Welcome message

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,8 @@
+//create a simple Linked List of 56, 30 and 70
+package com.bridgelabz;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Linked List Program.");
+    }
+}
